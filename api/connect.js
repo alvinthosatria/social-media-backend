38ad74a46b10eea5bf3de0941e4967bc40ff1e20
alvinthosatria <alvinthosatria@gmail.com>
2023-01-25 19:10:38 +0800

    message

--- api/connect.js
+++ api/connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Alvinthosatria2525",
-    database: "social"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b267ba3d8a7037",
+    password: "0e3cbcab",
+    database: "heroku_e04e0a65c8cdbef"
 })
