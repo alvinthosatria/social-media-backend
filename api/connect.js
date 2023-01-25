183ba9ea31971044d29d4c242e1890e69486a8ee
alvinthosatria <alvinthosatria@gmail.com>
2023-01-25 17:04:26 +0800

    redirect database endpoint

--- api/connect.js
+++ api/connect.js
@@ -4,5 +4,5 @@ export const db = mysql.createConnection({
     host: "us-cdbr-east-06.cleardb.net",
     user: "b267ba3d8a7037",
     password: "0e3cbcab",
-    database: "social"
+    database: "heroku_e04e0a65c8cdbef"
 })
